c2d9de123b5e3e0f55e2db7934a3254d180f38e4
soul-oge <exaucet.ockey@epitech.eu>
2023-10-30 17:11:59 +0100

    add user details style and correction data display

--- src/components/Dashboard/Score_Details.js
+++ src/components/Dashboard/Score_Details.js
@@ -22,17 +22,22 @@ const UserDetail = () => {
           userData.total_score += userData.all_score[category].categoryScore;
         }
         userData.subcategoryScores = {};
+        userData.categoryScores = {};
+
         for (const category in userData.all_score) {
-          for (const questionKey in userData.all_score[category]) {
-            const question = userData.all_score[category][questionKey];
-            const subcategory = question.sous_categorie;
-            if (!userData.subcategoryScores[subcategory]) {
-              userData.subcategoryScores[subcategory] = 0;
-            }
-            userData.subcategoryScores[subcategory] += question.score * question.coefficient;
+          if (!userData.categoryScores[category]) {
+            userData.categoryScores[category] = {};
           }
+        for (const questionKey in userData.all_score[category]) {
+          const question = userData.all_score[category][questionKey];
+          const subcategory = question.sous_categorie;
+          if (!userData.categoryScores[category][subcategory]) {
+            userData.categoryScores[category][subcategory] = 0;
+          }
+          userData.categoryScores[category][subcategory] += question.score * question.coefficient;
         }
-        setUserData(userData);
+      }
+      setUserData(userData);
       } else {
         console.log('Document de l\'utilisateur non trouvé.');
       }
@@ -41,33 +46,33 @@ const UserDetail = () => {
   }, []);
 
   return (
-    <div>
+    <div className="bg-gray-100 p-4">
       {userData ? (
-        <div>
-          <h1>Nom de l'utilisateur : {userData.Name}</h1>
-          <p>Score total : {userData.total_score}</p>
-          <h2>Scores par catégorie :</h2>
+        <div className="bg-white rounded p-4 shadow-md">
+          <h1 className="text-2xl font-bold mb-4">Nom de l'utilisateur : {userData.Name}</h1>
+          <p className="text-lg">Score total : {userData.total_score}</p>
+          <h2 className="text-lg mt-4 mb-2">Scores par catégorie :</h2>
           <ul>
             {Object.entries(userData.all_score).map(([category, data]) => (
-              <li key={category}>
-                {category}: {data.categoryScore}
-              </li>
-            ))}
-          </ul>
-          <h2>Somme par sous-catégorie :</h2>
-          <ul>
-            {Object.entries(userData.subcategoryScores).map(([subcategory, score]) => (
-              <li key={subcategory}>
-                {subcategory}: {score}
+              <li key={category} className="mb-2">
+                <span className="font-bold">{category}:</span> {data.categoryScore}
+                <h3 className="text-md mt-2 mb-1">Sous-catégories :</h3>
+                <ul>
+                  {Object.entries(userData.categoryScores[category]).map(([subcategory, score]) => (
+                    <li key={subcategory} className="mb-1">
+                      <span className="font-semibold">{subcategory}:</span> {score}
+                    </li>
+                  ))}
+                </ul>
               </li>
             ))}
           </ul>
         </div>
       ) : (
-        <p>Chargement en cours...</p>
+        <p className="text-xl">Chargement en cours...</p>
       )}
     </div>
-  );
+  );  
 };
 
 export default UserDetail;
